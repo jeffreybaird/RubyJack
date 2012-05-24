0ff1f1c7c36bd31a6b91842ee573eadc991d6578
jlbaird87 <jlbaird87@gmail.com>
2012-05-23 20:29:16 -0400

    first commit

---
+++ black_jack/lib/black_jack/player.rb
@@ -0,0 +1,115 @@
+require_relative 'deck'
+
+module BlackJack
+
+class Player
+  
+  attr_reader :name, :hand
+  attr_accessor :current_value, :hand_value
+  
+  def initialize (name)
+    @name = name
+    @hand = []
+    @current_value = 0
+  end
+  #I feel like I should have a to_s method, not sure I'm doing it right
+  def to_s
+   "#{@name}'s hand is worth #{self.hand_value}"
+  end
+  
+  def take_card deck
+    self.hand << deck.deal_a_card
+    self.hand
+  end
+  
+  def take_specific_card(deck=@deck, index_number=0)
+    self.hand << deck.deal_specific_card(deck, index_number)
+    self.hand
+  end
+  #this might be a bit redundant but I feel like it
+  #makes the code more intuitive
+  def hit deck
+    self.take_card(deck)
+  end
+  #Program interates through the hand calling the card_value
+  #of each card
+  def hand_value(current_value=0)
+    @current_value = current_value
+    self.hand.each do |card|
+      @current_value += card_value(card)
+    end
+    @current_value
+  end
+  #When the hand has a value of over 21 it busts and declares true 
+  #when it equals 21 it declares blackjack and returns true
+  def bust?
+   if hand_value > 21 
+     fold_hand
+     puts "#{@name} is Bust!"
+     true
+   elsif hand_value == 21
+     puts "BlackJack!"
+     true
+   else
+     puts "#{@name}'s hand is worth #{self.hand_value}"
+     false
+   end
+  end
+  #gives us a method for folding a busted hand
+  def fold_hand
+    @hand.each {|card| hand.delete(card)}
+    @hand = []
+  end  
+  #I hate this method and feel like there is a much easier
+  #way of doing it.
+  def card_value card
+    if card =~ /Queen/
+      10
+    elsif card =~ /King/
+      10
+    elsif card =~ /Jack/
+      10
+    elsif card =~ /Ace/
+      11
+    elsif card =~ /2/
+      2
+    elsif card =~ /3/
+      3
+    elsif card =~ /4/
+      4    
+    elsif card =~ /5/
+      5
+    elsif card =~ /6/
+      6
+    elsif card =~ /7/
+      7
+    elsif card =~ /8/
+      8
+    elsif card =~ /9/
+      9
+    elsif card =~ /10/
+      10
+    else
+      0
+    end
+  end
+  
+end  
+end
+
+if __FILE__ == $0
+deck = BlackJack::Deck.new
+deck.create_a_deck
+deck.shuffle_deck
+puts deck.new_deck
+
+player1 = BlackJack::Player.new("jeff")
+player2 = BlackJack::Player.new("clare")
+player1.take_card (deck)
+player2.take_card(deck)
+player1.take_card (deck)
+player2.take_card(deck)
+player1.hit(deck)
+puts "\n#{player1.name} has #{player1.hand} with a value of #{player1.hand_value}"
+puts "\n#{player2.name} has #{player2.hand} with a value of #{player2.hand_value}"
+end
---
+++ black_jack/spec/black_jack/game_spec.rb
@@ -0,0 +1,36 @@
+require 'black_jack/game'
+require 'black_jack/player'
+require 'black_jack/deck'
+module BlackJack
+
+describe Game do
+  before do
+    @game = BlackJack::Game.new("game1") 
+    @player = BlackJack::Player.new("jeff")
+    @dealer = BlackJack::Dealer.new("dealer")
+    @deck = BlackJack::Deck.new
+    @deck.create_a_deck
+  end
+  
+  it "has a name" do
+    @game.game_name.should_not be_nil
+  end
+  
+  it "adds a player to the game"
+    # @game.add_player(@player)
+    #     @game.each{|x| puts x}.should == "jeff's hand is worth 0"
+  
+  it "prints the players out"
+    # @game.add_player(@dealer).should == [dealers hand is worth 0]
+    #    @game.print_players.should == "dealer has []"
+  
+  
+  it "deals out a card" do
+    @game.take_card(@deck).should == ["King of diamonds"]
+  end
+  
+  it "can 'hit'" do
+    @game.hit(@deck).should == ["King of diamonds"]
+  end
+end
+end
